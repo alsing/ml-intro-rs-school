2968ad36eff0818e7298d6da56664a8b52eb1128
Alexandr Solomevich <alexandr.solomevich@gmail.com>
2022-05-08 22:47:07 +0300

    updated train.py

--- 9 - Evaluation Selection/src/ml_forest/train.py
+++ 9 - Evaluation Selection/src/ml_forest/train.py
@@ -15,26 +15,45 @@ from .pipeline import create_pipeline
     "--dataset-path",
     default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    show_default=True,
 )
 @click.option(
     "-s",
     "--save-model-path",
     default="data/model.joblib",
     type=click.Path(dir_okay=False, writable=True, path_type=Path),
+    show_default=True,
 )
-@click.option("--random-state", default=42, type=int)
+@click.option(
+    "--random-state",
+    default=42,
+    type=int,
+    show_default=True,
+)
 @click.option(
     "--test-split-ratio",
     default=0.2,
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
+    show_default=True,
 )
 @click.option(
     "--use-scaler",
     default=True,
     type=bool,
+    show_default=True,
 )
-@click.option("--max-iter", default=100, type=int)
-@click.option("--logreg-c", default=1.0, type=float)
+@click.option(
+    "--max-iter",
+    default=100,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--logreg-c",
+    default=1.0,
+    type=float,
+    show_default=True,
+)
 def train(
     dataset_path: Path,
     save_model_path: Path,
